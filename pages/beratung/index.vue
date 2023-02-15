de898d0b126500906dd1d55fed274ae57b2541c4
ulrichbenedikt <ulrichbenedikt@gmail.com>
2023-02-15 11:08:24 +0100

    bug: showing loading

--- pages/beratung/index.vue
+++ pages/beratung/index.vue
@@ -6,27 +6,36 @@
         {{ user.displayName }}
       </h1>
     </client-only>
-    <CoachFulfilRegistration />
-    <div v-if="
+    <CoachFulfilRegistration v-if="
+      user.topicArea == null ||
+      !stripeEnabled ||
+      user.verification != 'done'
+    "/>
+    <div v-else-if="
       user.topicArea &&
       stripeEnabled &&
       user.verification == 'done'
     ">
       <h2 class="primary--text mb-2">Anfragen</h2>
-      <v-btn outlined color="primary" class="mb-4" @click="loadMeetings"><v-icon small class="mr-1">mdi-update</v-icon>Nach Updates
+      <v-btn outlined color="primary" class="mb-4" @click="loadMeetings"><v-icon small
+          class="mr-1">mdi-update</v-icon>Nach Updates
         suchen</v-btn>
 
 
-      <CoachingRequestList :list="requests"/>
-      <div class="my-8 d-flex justify-center">
-        <v-btn text color="grey" @click="showOld = !showOld">vergangene Termine {{
-          showOld? "verbergen": "anzeigen"
-        }}</v-btn>
+      <div v-if="requests">
+        <CoachingRequestList :list="requests" />
+        <div class="my-8 d-flex justify-center">
+          <v-btn text color="grey" @click="showOld = !showOld">vergangene Termine {{
+            showOld? "verbergen": "anzeigen"
+          }}</v-btn>
+        </div>
+        <CoachingRequestList :list="requests" oldlist :showold="showOld" />
       </div>
-      <CoachingRequestList :list="requests" oldlist :showold="showOld" />
+      <div v-else><v-skeleton-loader type="article"></v-skeleton-loader></div>
     </div>
     <div v-else><v-skeleton-loader type="article"></v-skeleton-loader></div>
   </v-container>
+
 </template>
 
 <script>
